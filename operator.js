7c763894cfc91ca7a4023d4a021e25dd7474b046
unknown <1406076@naver.com>
2020-09-23 22:31:36 +0900

    test

---
+++ operator.js
@@ -0,0 +1,44 @@
+// 11. Loops
+let i = 3;
+while (i > 0) {
+  console.log(`while: ${i}`);
+  i--;
+}
+
+//do wihle
+// 조건이 맞는지 보다는 먼저 실행 후 조건을 따진다.
+do {
+  console.log(`do while: ${i}`);
+  i--;
+} while (i > 0);
+
+// for Loops, for(begin; condition; step)
+for (i = 3; i > 0; i--) {
+  console.log(`for : ${i}`);
+}
+
+for (let i = 3; i > 0; i = i - 2) {
+  //지역변수를 사용해도 좋다 let
+  console.log(`inline variable for: ${i}`);
+}
+
+// Q1. 0~10까지 출력하되. continue를 사용해서 만들기.
+for (let o = 0; o << 11; o++) {
+  if (o % 2 === 0) {
+    console.log(`q1. ${o}`);
+  }
+}
+
+// Q2. 0~10까지 출력하되. 숫자 8이 나오면 프로그램을 멈추게하기 break사용.
+for (let i = 0; i < 11; i++) {
+  if (i > 8) {
+    break;
+  }
+  console.log(`q2.${i}`);
+}
+
+for (let i = 1; i < 10; i++) {
+  for (let j = 1; j < 10; j++) {
+    console.log(`i: ${i} j: ${j}`);
+  }
+}
