a10f0c09e64beddf3a6e9869822efa34f676cf37
unknown <1406076@naver.com>
2020-09-23 22:36:34 +0900

    operator

--- operator.js
+++ operator.js
@@ -37,8 +37,4 @@ for (let i = 0; i < 11; i++) {
   console.log(`q2.${i}`);
 }
 
-for (let i = 1; i < 10; i++) {
-  for (let j = 1; j < 10; j++) {
-    console.log(`i: ${i} j: ${j}`);
-  }
-}
+console.log("ttt");
