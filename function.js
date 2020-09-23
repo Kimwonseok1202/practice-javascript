6c5f8e986ef738b4ee4252c2c651a63533f4f263
unknown <1406076@naver.com>
2020-09-24 01:03:27 +0900

    function test

--- function.js
+++ function.js
@@ -18,5 +18,26 @@ log(1234);
 // 2.Parameters
 // premitive 메모리에 value가 그대로 저장돼어 있기때문에 value가 저장되고
 // object 메모리에 레퍼런스가 저장이 돼서 레퍼런스가 전달됨
-// 5강 8:26초 부터 보기
-console.log("tt");
+
+function changeName(obj) {
+  obj.name = "coder";
+  obj.age = "20";
+}
+const ed = { name: "kim", age: "15" };
+changeName(ed);
+console.log(ed);
+
+// 3.Default parameters (added in ES6)
+function showMessage(message, from) {
+  console.log(`${message} by ${from}`);
+}
+showMessage("hi!", "hi@");
+
+// 4.Rest parameters (added in ES6)
+// ...은 Rest parameters로 배열형태로 전달된다.
+function printAll(...args) {
+  for (let i = 0; i < args.length; i++) {
+    console.log(args[i]);
+  }
+}
+console.log("a", "b", "c");
