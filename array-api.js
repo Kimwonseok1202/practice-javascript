6bb44075b5ffc653f7073eec67b1a1a1095ccce3
unknown <1406076@naver.com>
2020-10-15 19:29:15 +0900

    array-api test

--- array-api.js
+++ array-api.js
@@ -30,9 +30,91 @@
 // Q4. make new array without the first two elements
 {
   const array = [1, 2, 3, 4, 5];
-  const result = array.splice(0, 2);
+  const result = array.slice(2, 5);
+  console.clear();
   console.log(result);
   console.log(array);
 }
+// slice는 배열에서 원하는 부분만 가져올수 있다.
 
-console.log("array-api test");
+class Student {
+  constructor(name, age, enrolled, score) {
+    this.name = name;
+    this.age = age;
+    this.enrolled = enrolled;
+    this.score = score;
+  }
+}
+const student = [
+  new Student('A', 29, true, 45),
+  new Student('B', 28, false, 80),
+  new Student('C', 30, true, 90),
+  new Student('D', 40, false, 66),
+  new Student('E', 18, true, 88),
+];
+
+// Q5. find a student with the score 90 
+{
+  const result = student.find((student) => student.score === 90 );
+    console.log(result);
+}
+
+// Q6. make an array of enrolled students
+{
+  const result = student.filter((student) => student.enrolled);
+  console.log(result);
+  // filter는 조건을 부여한다.
+}
+
+//  Q7. make an array  containing only the student
+//     'scores result should be: {45, 80, 90, 66, 88}
+{
+  const result = student.map((student) =>student.score );
+  console.log(result);
+  // map은 받은 요소를 대체하게 바꿀수있다.
+}
+
+//  Q8. check if there is a student with the score lower than 50
+{
+  console.clear();
+  const result = student.some((student) => student.score < 50);
+  console.log(result);
+  // some 한개라도 조건을 만족하면 true을 출력한다.
+
+  const result2 = !student.every((student) => student.score < 50);
+  console.log(result2);
+  //  every는 모든 요소들이 조건을 만족하면 true를 출력한다.
+  // !를 쓰게되면 반대값이 출력된다.
+
+  // 이 문제에서는 some을 쓰는게 좋다. 조건에 따라 맞춰 쓰기
+}
+
+//  Q9. compute student's average score
+{
+  console.clear();
+  const result = student.reduce((prev, curr) => prev + curr.score, 0);
+  console.log(result / student.length);
+  // reduce는 배열의 값을 누적할때 사용
+}
+
+//  Q.10 make a string containing all the scores
+//  result should be : {'45, 80, 90, 66, 88}
+{
+  console.clear();
+  const result = student.map(student => student.score)
+  .filter(score => score >= 50)
+  .join();
+  console.log(result);
+}
+// 묶어서 사용이 가능하다.
+
+//  Bonus! do Q10 sorted in ascending order
+// result should be : {45, 66, 80, 88 ,90}
+{
+  const result =student.map(stduent => stduent.score)
+  .sort((a, b)=> a - b )
+  .join();
+  console.log(result);
+}
+
+console.log('array-api end');
